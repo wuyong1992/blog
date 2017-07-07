49c619c0a57cf34e4e7bc393b6531c7c6f05d6ca
wuyong1992 <627522616@qq.com>
2017-07-07 21:04:20 +0800

    blog

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,49 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {HeaderComponent} from './header/header.component';
+import {NavbarComponent} from './navbar/navbar.component';
+import {ContentComponent} from './content/content.component';
+import {SingleComponent} from './single/single.component';
+import {RightSideComponent} from './right-side/right-side.component';
+import {SearchComponent} from './search/search.component';
+import {PagingComponent} from './paging/paging.component';
+import {BlogDetailsComponent} from './blog-details/blog-details.component';
+import {FooterComponent} from './footer/footer.component';
+import {HomeComponent} from './home/home.component';
+import {RouterModule, Routes} from "@angular/router";
+import {FormsModule} from "@angular/forms";
+import {HttpModule} from "@angular/http";
+
+
+const routeConfig: Routes = [
+  {path: '', component: ContentComponent},
+  {path: 'blogDetail/:blogId', component: BlogDetailsComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NavbarComponent,
+    ContentComponent,
+    SingleComponent,
+    RightSideComponent,
+    SearchComponent,
+    PagingComponent,
+    BlogDetailsComponent,
+    FooterComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    RouterModule.forRoot(routeConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
