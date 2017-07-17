59a5058d363a233c3c95648f2200739eb27b8971
wuyong1992 <627522616@qq.com>
2017-07-17 18:06:58 +0800

    注册

--- src/app/user/service/user.service.ts
+++ src/app/user/service/user.service.ts
@@ -1,8 +1,31 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {User} from "../model/user-model";
+import {Http, URLSearchParams} from "@angular/http";
+import {Observable} from "rxjs/Observable";
+import 'rxjs/Rx';
 
 @Injectable()
 export class UserService {
 
-  constructor() { }
+  public responseData: Observable<any>;
+  public userRegisterURL = "http://localhost:8080/user/test";
+  public serverResponse: any;
 
+  constructor(private http:Http) {
+  }
+
+  //向后台post数据的写法如下
+  // let data = new URLSearchParams();
+  // data.append('email', user.email);
+  // data.append('password', user.password);
+  // return this.http.post(this.userRegisterURL,data);
+
+  public register(user: User) {
+    console.log("发送http.post请求");
+    let data = new URLSearchParams();
+    data.append("username", user.username);
+    data.append("nickname", user.nickname);
+    data.append("password", user.password);
+    return this.http.post(this.userRegisterURL, data).map(res => res.json());
+  }
 }
--- src/app/user/user-register/user-register.component.ts
+++ src/app/user/user-register/user-register.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {UserService} from "../service/user.service";
 import {FormBuilder, FormGroup} from "@angular/forms";
+import {User} from "../model/user-model";
 
 @Component({
   selector: 'app-user-register',
@@ -10,6 +11,7 @@ import {FormBuilder, FormGroup} from "@angular/forms";
 export class UserRegisterComponent implements OnInit {
 
   public userForm: FormGroup;
+  public user: User;
 
   constructor(private userService: UserService,
               private fb: FormBuilder) {
@@ -21,11 +23,27 @@ export class UserRegisterComponent implements OnInit {
   }
 
   onSubmit() {
-    console.log(this.userForm.value)
+    this.user = this.userForm.value;
+    console.log(this.user);
+    console.log("开始调用service");
+    /*this.user = user;
+     console.log(this.user)*/
+    this.userService.register(this.user)
+      .subscribe(
+      (data) => {
+        console.log(data);
+        if (data.status == 0) {
+          alert("注册成功");
+        }else {
+          alert("注册失败");
+        }
+      }
+    );
+    console.log("调用service结束");
   }
 
 
-  builder():void {
+  builder(): void {
     this.userForm = this.fb.group({
       username: [''],
       nickname: [''],
