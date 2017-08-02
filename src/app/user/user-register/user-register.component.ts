244bcc41062cef961617a88f11faae7cc22fec08
wuyong1992 <627522616@qq.com>
2017-08-02 18:21:06 +0800

    表单

--- src/app/user/user-register/user-register.component.ts
+++ src/app/user/user-register/user-register.component.ts
@@ -5,7 +5,8 @@ import {User} from "../model/user-model";
 import {ActivatedRoute, Router} from "@angular/router";
 import {ToastsManager} from "ng2-toastr";
 import {mobileValidator} from "../../validator/custom-validators";
-import {Http, URLSearchParams} from "@angular/http";
+import {Http, URLSearchParams, Headers} from "@angular/http";
+import 'rxjs/Rx';
 import {Observable} from "rxjs/Observable";
 
 @Component({
@@ -59,7 +60,7 @@ export class UserRegisterComponent implements OnInit {
             }
           },
           error => {
-            alert(error.message)
+            this.toastr.error(error.message+ "", "系统提示", {toastLife: 1500})
           }
         );
       console.log("调用service结束");
@@ -73,15 +74,16 @@ export class UserRegisterComponent implements OnInit {
 
   builder(): void {
     this.userForm = this.fb.group({
-      //['默认值',[校验器,校验器2,...]]
-      mobile: ['', mobileValidator, this.mobileAsyncValidator],
-      username: ['', [Validators.required, Validators.minLength(6)]],
-      password: ['']
+      //['默认值',[校验器,校验器2,...],异步校验]
+      // mobile: ['', mobileValidator, this.mobileAsyncValidator],
+      mobile: ['', mobileValidator],
+      username: ['', [Validators.required, Validators.minLength(6),Validators.maxLength(12)]],
+      password: ['',[Validators.required,Validators.minLength(6),Validators.maxLength(12)]]
     })
   }
 
 //异步校验，手机号是否已经被注册
-  mobileAsyncValidator(control: FormControl): any {
+  /*mobileAsyncValidator(control: FormControl): any {
 
     let flag = false;
 
@@ -98,7 +100,6 @@ export class UserRegisterComponent implements OnInit {
     let valid = myreg.test(control.value);
     // console.log("mobile校验结果:" + valid);
     return Observable.of(valid ? null : {mobile: {msg: "异步校验"}}).delay(2000);
-  }
-
+  }*/
 
 }
